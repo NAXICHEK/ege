75552db1b08c89b928efed691feed9157e29bef4
NAXICHEK <doge2007918@gmail.com>
2024-12-22 21:27:14 +0300

    разгрузка

--- 23/9964.py
+++ 23/9964.py
@@ -1,4 +1,12 @@
-def f(c, e):
+from functools import lru_cache
+
+a = ''
+@lru_cache(None)
+def f(c, e, l):
+    global a
     if c > e: return 0
     if c == e: return 1
-    if c < e: return f(c+1, e) + f(c*3, e) + f(c+5, e)
+    if c < e: return f(c+1, e, 'a') + f(c*3, e, 'b') + f(c+5, e, 'c')
+    a += l
+
+print(f(3, 69, 'g') if 'cac' in a else print(0))
