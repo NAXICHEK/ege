14a1775c32295a36ac03639f3cf9ac3dd055a5d0
NAXICHEK <doge2007918@gmail.com>
2024-12-24 22:11:46 +0300

    выгрузка

--- 25/13868_1.py
+++ 25/13868_1.py
@@ -1,4 +1,1 @@
-from fnmatch import *
-for x in range(2024, 10**10, 2024):
-    if fnmatch(str(x), '112?57*4') and sum(map(int, str(x))) % 2 == 1:
-        print(x, x//2024)
+from fnmatch import fnmatch; [print(x, x//2024) for x in range(2024, 10**10, 2024) if fnmatch(str(x), '112?57*4') and sum(map(int, str(x))) % 2 == 1]
