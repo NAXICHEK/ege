97cce1294352756c12d6560f13c1c501d30c17a8
NAXICHEK <doge2007918@gmail.com>
2024-12-27 10:09:36 +0300

    выгрузка

---
+++ var/27.12.1.k/12.12921.py
@@ -0,0 +1,11 @@
+for i in range(4, 10000):
+    s = '5' + '2'*i
+    while '52' in s or '2222' in s or '1122' in s:
+        s = s.replace('52', '11', 1)
+        s = s.replace('2222', '5', 1)
+        s = s.replace('1122', '25', 1)
+    if str(sum(map(int, s)))[-1] == '7':
+        print(i)
+        print(sum(map(int, s)))
+        print(s)
+        break
