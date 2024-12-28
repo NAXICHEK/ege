f409e3c801216c0beb74fd860288c7ec166823be
NAXICHEK <doge2007918@gmail.com>
2024-12-28 20:38:21 +0300

    выгрузка

--- 1.py
+++ 1.py
@@ -1,5 +1,15 @@
-a = '12344'
-b = set(a)
-print(b)
-print(*sorted('АРГУМЕНТ'))
-print(ord('С'))
+from itertools import *
+c = 1
+
+# А Г Е М Н Р Т У
+
+for x in product(sorted('АРГУМЕНТ'), repeat=4):
+    d = 0
+    s = ''.join(x)
+    if len(s) == len(set(s)):
+        for i in range(len(s)-1):
+            if ord(s[i]) < ord(s[i+1]):
+                d += 1
+    if d == 3:
+        print(c)
+    c += 1
