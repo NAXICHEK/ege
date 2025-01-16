6090e027add7dc088dbab8efad15f6b48d4ca45a
NAXICHEK <doge2007918@gmail.com>
2025-01-16 21:09:52 +0300

    выгрузка

--- 24/18029/18029.py
+++ 24/18029/18029.py
@@ -1,11 +1,9 @@
 f = open('24_18029.txt').read()
-f = f.replace('Y', '-')
-f = f.replace('Z', ',')
 f = f.replace('X', ' ').split()
 m = 0
 g = []
 for i in f:
-    g.append([i.count('-'), len(i)+2])
+    g.append([i.count('Y'), len(i) + 2])
 for i in g:
     a = i[0]
     m = max(m, a)
