d5d9cdebfa5ea3f6b02f842f80e548bf5af57caf
NAXICHEK <doge2007918@gmail.com>
2025-02-28 15:53:34 +0300

    выгрузка

---
+++ 9/18364/18364.py
@@ -0,0 +1,13 @@
+f = open('18364')
+c = 0
+for s in f:
+    a = [int(x) for x in s.split()]
+    ar = [x for x in a if a.count(x) > 1]
+    a1 = [x for x in a if a.count(x) == 1]
+    if ar:
+        g = 1
+        for i in ar:
+            g *= i
+        if sum(a1)*3 <= g:
+            c += 1
+print(c)
