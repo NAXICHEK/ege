771352b5ed85d39c9f2b199bed81626f75239191
NAXICHEK <doge2007918@gmail.com>
2025-03-10 08:33:27 +0300

    выгрузка

--- 1.py
+++
@@ -1,7 +0,0 @@
-from itertools import combinations
-
-from aiogram.utils.markdown import blockquote
-
-a = [1, 2, 3, 4, 5, 6]
-for x in combinations(a, 3):
-    print(x)
--- 16/17557.py
+++ 16/17557.py
@@ -4,5 +4,5 @@ setrecursionlimit(9999)
 def f(n):
     if n == 1: return 1
     if n > 1: return 2 * n * f(n-1)
-print((f(2024)//16 - f(2023))//f(2022))
-print(f(2024)%16)
+# print((f(2024)//16 - f(2023))//f(2022))
+# print(2 * 2023 * (((2 * 2024) / 16) - 1))
