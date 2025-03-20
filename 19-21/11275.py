1be340d78425adc3c7dd48adaa709e51a7197145
NAXICHEK <doge2007918@gmail.com>
2025-03-20 20:46:39 +0300

    выгрузка

--- 19-21/11275.py
+++ 19-21/11275.py
@@ -1,14 +1,14 @@
-def f(a, b, m):
+def fn(a, b, m):
     if a+b >= 131: return m % 2 == 0
     if m == 0: return 0
     h = [
-        f(a+2, b, m-1),
-        f(a, b+2, m-1),
-        f(a*2, b, m-1),
-        f(a, b*2, m-1),
+        fn(a+2, b, m-1),
+        fn(a, b+2, m-1),
+        fn(a*2, b, m-1),
+        fn(a, b*2, m-1),
     ]
     return any(h) if (m-1) % 2 == 0 else all(h)
 
-print([s for s in range(1, 123) if not f(11, s, 1) and f(11, s, 2)]) # 30
-print([s for s in range(1, 123) if not f(11, s, 1) and f(11, s, 3)]) # 54 57
-print([s for s in range(1, 123) if not f(11, s, 2) and f(11, s, 4)]) # 52
+print([s for s in range(1, 123) if not fn(11, s, 1) and fn(11, s, 2)]) # 30
+print([s for s in range(1, 123) if not fn(11, s, 1) and fn(11, s, 3)]) # 54 57
+print([s for s in range(1, 123) if not fn(11, s, 2) and fn(11, s, 4)]) # 52
