597adb9f74c2bb2e7de45ca523792c4d44008f7d
NAXICHEK <doge2007918@gmail.com>
2025-03-23 20:21:40 +0300

    выгрузка

---
+++ 7/15319.py
@@ -0,0 +1,11 @@
+
+
+
+from math import *
+for i in range(1000):
+    a = 2764*1793*log2(7026)
+    paket = (a*148/8)
+    if paket <= 18349566/8*i:
+        print(i)
+        break
+print(9372587604.354164/18349566)
