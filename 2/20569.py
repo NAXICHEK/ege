f25fc2e0f406b7d82b6043d1b8344887a1d6e5b1
NAXICHEK <doge2007918@gmail.com>
2025-04-05 20:24:22 +0300

    решил задачу (крутой)

---
+++ 2/20569.py
@@ -0,0 +1,6 @@
+print('w x y z f')
+for w in 0,1:
+    for x in 0,1:
+        for y in 0,1:
+            for z in 0,1:
+                print(w, x, y, z, 1 if ((w <= z) == (x <= (not y))) and (x or z) else 0)
