26f336c5ebaf0d055b9da1850415043e04907423
NAXICHEK <doge2007918@gmail.com>
2025-04-17 20:29:04 +0300

    решил задачу (крутой)

--- 1.py
+++
@@ -1,7 +0,0 @@
-def div(x):
-    d = set()
-    for i in range(2, int(x**0.5) + 1):
-        if x % i == 0:
-            d |= {i, x//i}
-    return sorted(d)
-print(div(100))
---
+++ 13/19748.py
@@ -0,0 +1,14 @@
+from ipaddress import *
+# for i in range(1, 33):
+#     net1 = ip_network(f'157.220.185.237/{i}', 0)
+#     net2 = ip_network(f'157.220.184.230/{i}', 0)
+#     if net1 == net2:
+#         c = 0
+#         print(net1)
+net1 = ip_network(f'157.220.185.237/23', 0)
+c = 0
+for j in net1:
+    b = f'{j:b}'
+    if b.count('1') == 15:
+        c += 1
+print(c)
