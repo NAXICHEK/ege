abebe1207ef539bed3e851716f095e6c8a4ec470
naxikasus <doge2007918@gmail.com>
2025-05-31 17:14:15 +0300

    решил задачу

--- 1.py
+++ 1.py
@@ -1,2 +1,6 @@
-a = '1234'
-print(a[2:])
+for w in 0,1:
+    for x in 0,1:
+        for y in 0,1:
+            for z in 0,1:
+                if ((z <= y) or ((w <= x) <= y)) == 0:
+                    print(w, x, y, z)
